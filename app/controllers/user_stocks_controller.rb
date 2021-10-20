471c17c3fbaa047d274a4f7c9d3ae75bb090086f
Derek Finch <sagesolar@gmail.com>
2021-10-20 17:18:17 +0100

    Added friendships for users

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -14,7 +14,7 @@ class UserStocksController < ApplicationController
   end
 
   def destroy
-    stock = Stock.find(params[:id])lancashi
+    stock = Stock.find(params[:id])
     user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
     user_stock.destroy
     flash[:notice] = "#{stock.ticker} was successfully removed from your portfolio"
